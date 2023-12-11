813058325573dca2ac9e6f68eb5f5551630098be
Official0mega <supr3m30m3ga@gmail.com>
2023-12-11 14:54:52 +0100

    updated Password Manager

--- CodeVerse Password Manager/APPNAME/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import home
-from . import views
-
-urlpatterns = [
-    path("", home, name="home"),
-    path('msg.html', views.new_page_view, name='new_page_view'),
-]
